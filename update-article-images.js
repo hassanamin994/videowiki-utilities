ad144b222a02378a8deff77d7f239f99e2304bf3
hassanamin994 <hassanmamin994@gmail.com>
2019-04-28 07:10:23 +0200

    add update article images script

---
+++ update-article-images.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL;
+const args = process.argv.slice(2);
+const lang = args[0];
+
+const { Article, Video, UploadFormTemplate, Humanvoice } = require('./models');
+const dbCOnnection = `${DB_CONNECTION_URL}-${lang}`;
+mongoose.connect(dbCOnnection) // connect to our mongoDB database //TODO: !AA: Secure the DB with authentication keys
+
+const articlesImages = [
+    { title: 'Wikipedia:VideoWiki/Dengue_fever_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Denguerash.JPG/400px-Denguerash.JPG' },
+    { title: 'Wikipedia:VideoWiki/Pneumonia_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg/400px-Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg' },
+    { title: 'Wikipedia:VideoWiki/Typhoid_fever_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Salmonella_typhi_typhoid_fever_PHIL_2215_lores.jpg/400px-Salmonella_typhi_typhoid_fever_PHIL_2215_lores.jpg' },
+    { title: 'Wikipedia:VideoWiki/Cancer_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Tumor_Mesothelioma2_legend.jpg/400px-Tumor_Mesothelioma2_legend.jpg' },
+    { title: 'Wikipedia:VideoWiki/Measles_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/RougeoleDP.jpg/400px-RougeoleDP.jpg' },
+    { title: 'Wikipedia:VideoWiki/Hepatitis_C_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Liver2.png/400px-Liver2.png' },
+    { title: 'Wikipedia:VideoWiki/Major_depressive_disorder_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Van_Gogh_-_Trauernder_alter_Mann.jpeg/400px-Van_Gogh_-_Trauernder_alter_Mann.jpeg' },
+    { title: 'Wikipedia:VideoWiki/Tuberculosis_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Tuberculosis-x-ray-1.jpg/400px-Tuberculosis-x-ray-1.jpg' },
+    { title: 'Wikipedia:VideoWiki/Malaria_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Malaria_Parasite_Connecting_to_Human_Red_Blood_Cell_%2834034143483%29.jpg/400px-Malaria_Parasite_Connecting_to_Human_Red_Blood_Cell_%2834034143483%29.jpg' },
+    { title: 'Wikipedia:VideoWiki/Hypertension_overview', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Blood_pressure_testing.jpg/400px-Blood_pressure_testing.jpg' },
+    { title: 'विकिपीडिया:वीडियोविकि/डेंगू_बुख़ार', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Denguerash.JPG/400px-Denguerash.JPG' },
+    { title: 'विकिपीडिया:वीडियोविकि/निमोनिया', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg/400px-Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg' },
+    { title: 'विकिपीडिया:वीडियोविकि/आंत्र_ज्वर', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Salmonella_typhi_typhoid_fever_PHIL_2215_lores.jpg/400px-Salmonella_typhi_typhoid_fever_PHIL_2215_lores.jpg' },
+    { title: 'विकिपीडिया:वीडियोविकि/कैंसर', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Tumor_Mesothelioma2_legend.jpg/400px-Tumor_Mesothelioma2_legend.jpg' },
+    { title: 'विकिपीडिया:वीडियोविकि/खसरा', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/RougeoleDP.jpg/400px-RougeoleDP.jpg' },
+  
+]
+
+function updateArticleImages(articlesImages) {
+    articlesImages.forEach(({ title, image }) => {
+        Article.updateMany({ title }, { $set: { image } }, (err, result) => {
+            console.log(title, err, result);
+        })
+    })
+    
+}
+
+updateArticleImages(articlesImages.map(t => ({ ...t, title: decodeURIComponent(t.title) })))
